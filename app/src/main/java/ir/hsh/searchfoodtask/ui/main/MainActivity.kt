f0a7c749f4c4fd9536b000bd67c96f5b9637500c
sp itik <6252275-tikapp@users.noreply.gitlab.com>
2021-08-19 01:04:19 +0430

    Add Packages

--- app/src/main/java/ir/hsh/searchfoodtask/ui/main/MainActivity.kt
+++ app/src/main/java/ir/hsh/searchfoodtask/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package ir.hsh.searchfoodtask
+package ir.hsh.searchfoodtask.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ir.hsh.searchfoodtask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
